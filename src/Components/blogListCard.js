969418690f13e991e9a277d0b0ed136f212e959b
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-11-07 09:56:53 -0500

    Realized I never commited and submitted assignment

--- src/Components/blogListCard.js
+++ src/Components/blogListCard.js
@@ -3,7 +3,7 @@ const BlogListCard = (props) => {
         <div>
             <h2>{props.blog.title}</h2>
             <p>{props.blog.author}</p>
-            <p>{props.blog.text}</p>
+            <p>Text Here</p>
             <p>{props.blog.id}</p>
             <p>{props.blog.createdAt}</p>
         </div>
